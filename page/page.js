7e84080cedf134b0ce4e2d1f35ecdf2ba90d6f20
Will Toher <will.toher.0@gmail.com>
2017-06-17 16:22:07 -0500

    Better list and favicons

--- page/page.js
+++ page/page.js
@@ -1,14 +1,36 @@
 
-function addTabs(tabs) {
-    var tabsListNode = document.getElementById("tablist");
+function generateListItem(tab, parent) {
+    var item = document.createElement("li");
+    item.classList.add("tabItem");
 
-    tabTitleText = document.createTextNode("List of Tabs:");
+    var img = document.createElement("img");
+    img.src = tab.favIconUrl;
+    img.width = "32";
+    img.height = "32";
+
+
+    item.appendChild(img);
+   
+    item.appendChild(document.createTextNode(tab.title));
+
+    parent.appendChild(item);
+}
+
+function addTabs(tabs) {
+    var tabsListNode = document.getElementById("tabList");
+
+    var tabTitleText = document.createTextNode("List of Tabs:");
     tabsListNode.appendChild(tabTitleText);
 
     for (let tab of tabs) {
-        console.log(tab.title)
-        var tabitem = document.createTextNode(tab.title);
-        tabsListNode.appendChild(tabitem);
+        //console.log(tab.title)
+
+        //var tabListItem = document.createElement("li");
+        //tabListItem.innerText = tab.title;
+
+        //tabsListNode.appendChild(tabListItem);
+
+        generateListItem(tab, tabsListNode);
     }
 }
 
@@ -16,7 +38,11 @@ function onError(error) {
     console.log(`Error: ${error}`);
 }
 
-var currWindowTabs = browser.tabs.query({currentWindow: true});
-currWindowTabs.then(addTabs, onError)
 
-console.log("Done")
+window.onload = function () {
+    var currWindowTabs = browser.tabs.query({currentWindow: true});
+    currWindowTabs.then(addTabs, onError)
+
+    console.log("Done")
+}
+
