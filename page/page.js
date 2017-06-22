087c76e8e93351e75b206bb43f8348be4747b18b
Will Toher <will.toher.0@gmail.com>
2017-06-21 19:57:01 -0500

    Preliminary form-based functionality added (it does a thing now)

--- page/page.js
+++ page/page.js
@@ -1,4 +1,3 @@
-
 // GLOBAL STATE
 var windowNumber = 1;
 
@@ -9,71 +8,109 @@ var windowNumber = 1;
 // If this div doesn't exist, creates it and appends it to "windowsContainer"
 // Parent div Id is of the form "window-[window Id]"
 function getParent(tab) {
-    var parent = document.getElementById("window-" + tab.windowId);
-    if (parent === null) {
-        parent = document.createElement("div");
-        parent.id = "window-" + tab.windowId;
-        parent.classList.add("window");
-        document.getElementById("windowsContainer").appendChild(parent);
-
-        var title = document.createElement("div");
-        title.classList.add("windowTitle");
-        var titleText = document.createElement("h2");
-        titleText.textContent = "Window " + windowNumber;
-        title.appendChild(titleText);
-        parent.appendChild(title);
-        windowNumber++;
-    }
-    return parent;
+  var parent = document.getElementById("window-" + tab.windowId);
+  if (parent === null) {
+    parent = createWindowDiv(tab.windowId);
+  }
+  return parent;
 }
 
+function createWindowDiv(windowId) {
+  var newWindow = document.createElement("div");
+  newWindow.id = "window-" + windowId;
+  newWindow.classList.add("window");
+  document.getElementById("windowsContainer").appendChild(newWindow);
 
-// TODO: move creation logic from getParent here
-function createWindowDiv(windowId) {}
+  var title = document.createElement("div");
+  title.classList.add("windowTitle");
+  var titleText = document.createElement("h2");
+  titleText.textContent = "Window " + windowNumber;
+  title.appendChild(titleText);
+  newWindow.appendChild(title);
 
+  var windowOpt = document.createElement("option");
+  windowOpt.value = windowId;
+  windowOpt.innerText = "Window " + windowNumber;
+  document.getElementById("windowPicker").appendChild(windowOpt);
+  
+  windowNumber++;
+
+  return newWindow;
+}
 
 function generateListItem(tab, parent) {
-    var item = document.createElement("div");
-    item.classList.add("tabItem");
+  var item = document.createElement("div");
+  item.classList.add("tabItem");
 
-    var img = document.createElement("img");
-    img.src = tab.favIconUrl;
-    console.log(img.src);
-    if (img.src.endsWith("undefined")) {
-        // TODO: Replace temp art
-        // Credit: https://openclipart.org/detail/194751/globe
-        img.src = "globe-300px.png";
-    }
-    img.width = "32";
-    img.height = "32";
-    item.appendChild(img);
-    
-    var itemText = document.createElement("span");
-    itemText.classList.add("itemText");
-    itemText.textContent = tab.title;
-    item.appendChild(itemText);
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = "checkbox-" + tab.id;
+  checkbox.classList.add("tabCheckbox");
+  checkbox.name = tab.id;
+  // checkbox.value is unset, so default value "on" will be sent
+  item.appendChild(checkbox);
 
-    parent.appendChild(item);
+  var img = document.createElement("img");
+  img.src = tab.favIconUrl;
+  if (img.src.endsWith("undefined")) {
+    // TODO: Replace temp art
+    // Credit: https://openclipart.org/detail/194751/globe
+    img.src = "globe-300px.png";
+  }
+  img.width = "32";
+  img.height = "32";
+  img.classList.add("tabFavicon")
+  item.appendChild(img);
+
+  var itemText = document.createElement("span");
+  itemText.classList.add("itemText");
+  itemText.textContent = tab.title;
+  item.appendChild(itemText);
+
+  parent.appendChild(item);
 }
-
 
 function addTabs(tabs) {
-    for (let tab of tabs) {
-        var parent = getParent(tab);
-        generateListItem(tab, parent);
-    }
+  for (let tab of tabs) {
+    var parent = getParent(tab);
+    generateListItem(tab, parent);
+  }
 }
 
+function moveSelectedTabs() {
+  // Get ID of target window
+  let windowPicker = document.getElementById("windowPicker");
+  let targetId = windowPicker[windowPicker.selectedIndex].value;
+
+  if (targetId === "new") { 
+    console.log("Can't move to new window yet!");
+    return;
+  } else {
+    targetId = parseInt(targetId);
+  }
+
+  // Get checked boxes
+  let checkedBoxes = document.getElementsByTagName("input");
+  for (let box of checkedBoxes) {
+    if (box.checked){
+      var moving = browser.tabs.move(parseInt(box.name), {windowId: targetId, index: -1}); 
+    }
+  }
+}
+
+function onMoved(tab) {
+  console.log(`Moved: ${tab}`);
+}
 
 function onError(error) {
-    console.log(`Error: ${error}`);
+  console.log(`Error: ${error}`);
 }
 
+window.onload = function() {
+  var currWindowTabs = browser.tabs.query({});
+  currWindowTabs.then(addTabs, onError);
 
-window.onload = function () {
-    var currWindowTabs = browser.tabs.query({});
-    currWindowTabs.then(addTabs, onError);
+  document.getElementById("moveSelectedBtn").addEventListener("click", moveSelectedTabs);
 
-    console.log("Done")
-}
-
+  console.log("Done");
+};
