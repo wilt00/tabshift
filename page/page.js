372b14ca4e840d5dd3cdd3c3d94bbd7bac46a8fa
Will Toher <will.toher.0@gmail.com>
2017-06-22 20:06:01 -0500

    Async! Also, refresh after move

--- page/page.js
+++ page/page.js
@@ -4,6 +4,14 @@ var windowNumber = 1;
 // Drag selection example: https://codepen.io/netgfx/pen/twAfG
 // Explanation: http://nightlycoding.com/index.php/2014/02/click-and-drag-multi-selection-rectangle-with-javascript/
 
+function onMoved(tab) {
+  console.log(`Moved: ${tab}`);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
 // Returns reference to div where given tab should go
 // If this div doesn't exist, creates it and appends it to "windowsContainer"
 // Parent div Id is of the form "window-[window Id]"
@@ -32,7 +40,7 @@ function createWindowDiv(windowId) {
   windowOpt.value = windowId;
   windowOpt.innerText = "Window " + windowNumber;
   document.getElementById("windowPicker").appendChild(windowOpt);
-  
+
   windowNumber++;
 
   return newWindow;
@@ -40,6 +48,7 @@ function createWindowDiv(windowId) {
 
 function generateListItem(tab, parent) {
   var item = document.createElement("div");
+  item.id = "item-" + tab.id;
   item.classList.add("tabItem");
 
   var checkbox = document.createElement("input");
@@ -47,7 +56,6 @@ function generateListItem(tab, parent) {
   checkbox.id = "checkbox-" + tab.id;
   checkbox.classList.add("tabCheckbox");
   checkbox.name = tab.id;
-  // checkbox.value is unset, so default value "on" will be sent
   item.appendChild(checkbox);
 
   var img = document.createElement("img");
@@ -59,7 +67,7 @@ function generateListItem(tab, parent) {
   }
   img.width = "32";
   img.height = "32";
-  img.classList.add("tabFavicon")
+  img.classList.add("tabFavicon");
   item.appendChild(img);
 
   var itemText = document.createElement("span");
@@ -77,40 +85,53 @@ function addTabs(tabs) {
   }
 }
 
-function moveSelectedTabs() {
+function moveTab(box, targetId) {
+  console.log("Moving tab: " + box.name);
+  if (box.checked) {
+    return browser.tabs.move(parseInt(box.name), {
+      windowId: targetId,
+      index: -1
+    });
+  }
+}
+
+async function moveSelectedTabs() {
   // Get ID of target window
-  let windowPicker = document.getElementById("windowPicker");
+  const windowPicker = document.getElementById("windowPicker");
   let targetId = windowPicker[windowPicker.selectedIndex].value;
 
-  if (targetId === "new") { 
-    console.log("Can't move to new window yet!");
+  if (targetId === "new") {
+    // TODO: Create new window
+    window.alert("Can't move to new window yet!");
     return;
   } else {
     targetId = parseInt(targetId);
   }
 
   // Get checked boxes
-  let checkedBoxes = document.getElementsByTagName("input");
-  for (let box of checkedBoxes) {
-    if (box.checked){
-      var moving = browser.tabs.move(parseInt(box.name), {windowId: targetId, index: -1}); 
-    }
-  }
+  const checkedBoxes = document.querySelectorAll(
+    "input[type=checkbox]:checked"
+  );
+  const checkedBoxesArr = Array.from(checkedBoxes);
+
+  // Shoutout to https://gist.github.com/yesvods/01fbeeb39de2c9d16a0a
+  let moves = checkedBoxesArr.map(box => moveTab(box, targetId));
+  let results = await Promise.all(moves);
+
+  console.log("After results");
+
+  browser.tabs.reload();
 }
 
-function onMoved(tab) {
-  console.log(`Moved: ${tab}`);
-}
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-window.onload = function() {
+function loadTabs() {
   var currWindowTabs = browser.tabs.query({});
   currWindowTabs.then(addTabs, onError);
 
-  document.getElementById("moveSelectedBtn").addEventListener("click", moveSelectedTabs);
+  document
+    .getElementById("moveSelectedBtn")
+    .addEventListener("click", moveSelectedTabs);
 
-  console.log("Done");
-};
+  console.log("Loaded");
+}
+
+window.onload = loadTabs;
